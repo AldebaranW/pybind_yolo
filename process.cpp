6cf0574e4d7f7677dfab3c9d84f3b9554f23cbf3
AldebaranW <wupl20040501@163.com>
2023-03-06 18:03:22 +0800

    gitignore

--- process.cpp
+++ process.cpp
@@ -10,6 +10,7 @@ void dataprocess(std::vector<double> box, std::vector<double> mask) {
     // TODO: 处理识别到的数据
     // box: 长度为6的数组 [x, y, x, y, 置信度， 类别]
     // mask: 长度不定， 形式为 [[点1坐标]， [点2坐标]， ...]
+    
 }
 
 PYBIND11_MODULE(_process, m) {
